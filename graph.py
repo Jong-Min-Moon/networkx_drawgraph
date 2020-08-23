acb6a0db54f48fdbd85934ed85ead11b752d4a68
Guest <guest-rdbogk@h24556.sgb.skir.kr>
2020-08-23 18:06:33 +0900

    노드 사이즈 반영 성공
    Signed-off-by: Guest <guest-rdbogk@h24556.sgb.skir.kr>

--- graph.py
+++ graph.py
@@ -9,9 +9,7 @@ from matplotlib import rc
 font_name = fm.FontProperties(fname = 'NanumBarunGothic.ttf').get_name()
 rc('font', family = font_name)
 
- 
 
-#networkx의 node의 라벨은 따로 지정을 해줘야함.
 
 
 
@@ -20,13 +18,23 @@ rc('font', family = font_name)
 G1 = nx.Graph()
 
 words_and_weights = pd.read_csv('mytable.csv')
-print(words_and_weights)
+weights = (words_and_weights.weights * 600).astype('int')
+weights = pd.concat( [pd.Series([3000]), weights] )
+weights = weights.to_numpy()
+print(weights)
 edges = [('김정은', word) for word in words_and_weights.words]
-print(edges)
-# add node/edge pairs
-G1.add_edges_from(edges)
-pos=nx.shell_layout(G1)
+G1.add_edges_from(edges)# add node/edge pairs
+
+#node attributes
+# node_attr = words_and_weights.set_index('words').to_dict('index')
+# node_attr['김정은'] = {'weights' : 300}
+# nx.set_node_attributes(G1, node_attr)
+
+#position
+pos = nx.spring_layout(G1)
+
 # draw the network G1
-nx.draw_networkx(G1, pos = pos, with_labels = False)
-nx.draw_networkx_labels(G1, pos, font_family = font_name, font_size=10)
+nx.draw_networkx(G1, pos = pos, node_size = weights, with_labels = False)
+nx.draw_networkx_labels(G1, pos, font_family = font_name, font_size=10) #networkx의 node의 라벨은 따로 지정을 해줘야함.
+
 plt.show()
--- table.py
+++ table.py
@@ -3,7 +3,9 @@ import pandas as pd
 table = pd.DataFrame()
 
 words = ['강아지', '고양이', '토끼', '거북이', '호랑이']
-weights = [0.2, 0.3, 0.4, 0.5, 0.3]
+words = [word + str(i) for word in words for i in range(10)]
+
+weights = [0.1, 0.2, 0.3, 0.4, 0.5] * 10
 
 table['words'] = words
 table['weights'] = weights
