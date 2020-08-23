476c7e5a3b4a95c306739609b23cf0f8275861be
Guest <guest-qgjgaa@h27286.sgb.skir.kr>
2020-08-23 22:38:49 +0900

    감성에 따라 색깔 바뀌는 것 완료
    Signed-off-by: Guest <guest-qgjgaa@h27286.sgb.skir.kr>

--- graph.py
+++ graph.py
@@ -4,7 +4,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.font_manager as fm
 from matplotlib import rc
-
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import MaxAbsScaler
 #이렇게 하면 matplotlib의 title axis의 한글이 정상적으로 나옴.
 font_name = fm.FontProperties(fname = 'NanumBarunGothic.ttf').get_name()
 rc('font', family = font_name)
@@ -17,13 +18,29 @@ rc('font', family = font_name)
 
 G1 = nx.Graph()
 
-words_and_weights = pd.read_csv('mytable.csv')
-weights = (words_and_weights.weights * 600).astype('int')
-weights = pd.concat( [pd.Series([3000]), weights] )
-weights = weights.to_numpy()
-print(weights)
-edges = [('김정은', word) for word in words_and_weights.words]
+
+
+words = pd.read_csv('mytable.csv')
+
+edges = [('김정은', word) for word in words.words]
 G1.add_edges_from(edges)# add node/edge pairs
+
+scaler = MinMaxScaler()
+weights = words.weights.to_numpy()
+weights = scaler.fit_transform(weights.reshape(-1,1))
+weights = weights.flatten()
+weights = (2000 * weights + 500).astype('int')
+weights= np.insert(weights, 0, 3200)
+print(weights)
+
+scaler = MaxAbsScaler()
+sentiment = words.sentiment.to_numpy()
+sentiment = scaler.fit_transform(sentiment.reshape(-1,1))
+sentiment = sentiment.flatten()
+sentiment= np.insert(sentiment, 0, 0)
+sentiment = -1 * sentiment
+print(sentiment)
+
 
 #node attributes
 # node_attr = words_and_weights.set_index('words').to_dict('index')
@@ -34,7 +51,7 @@ G1.add_edges_from(edges)# add node/edge pairs
 pos = nx.spring_layout(G1)
 
 # draw the network G1
-nx.draw_networkx(G1, pos = pos, node_size = weights, with_labels = False)
-nx.draw_networkx_labels(G1, pos, font_family = font_name, font_size=10) #networkx의 node의 라벨은 따로 지정을 해줘야함.
+nx.draw_networkx(G1, pos = pos, node_size = weights, node_color = sentiment, with_labels = False, cmap = plt.cm.get_cmap('coolwarm'))
+nx.draw_networkx_labels(G1, pos, font_family = font_name, font_size=11) #networkx의 node의 라벨은 따로 지정을 해줘야함.
 
 plt.show()
--- table.py
+++ table.py
@@ -6,8 +6,10 @@ words = ['강아지', '고양이', '토끼', '거북이', '호랑이']
 words = [word + str(i) for word in words for i in range(10)]
 
 weights = [0.1, 0.2, 0.3, 0.4, 0.5] * 10
-
+sentiment = [-0.1, -0.2, -0.3, -0.4, -0.5, 0, 0.1, 0.2, 0.3, 0.4] * 5
 table['words'] = words
 table['weights'] = weights
+table['sentiment'] = sentiment
 
+print(table)
 table.to_csv('mytable.csv', index = False)
